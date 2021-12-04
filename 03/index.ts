3d1e8a76b8ec66448810b5be22666867e057ff7a
Seether85 <seether85@gmail.com>
2021-12-05 00:41:03 +0100

    Day 03 part 2;

--- 03/index.ts
+++ 03/index.ts
@@ -3,6 +3,8 @@ import { readFileSync } from 'fs'; // VSCode will complain, node will not
 type sonarInput = string[];
 type gammaRate = number[];
 type epsilonRate  = number[];
+type oxygenRate  = number[];
+type CO2Rate  = number[];
 
 const input = readFileSync('./input.txt', 'utf-8').split('\r\n')
 
@@ -18,7 +20,7 @@ const gammaRate = (sonarInput: sonarInput): gammaRate => {
     }, new Array(sonarInput[0].length).fill(0));
     
     digitSplitSonarInput.forEach((v, i) => 
-        arrayGammaRate[i] = v > sonarInput.length / 2 ? 1 : 0
+        arrayGammaRate[i] = v >= sonarInput.length / 2 ? 1 : 0
     );
 
     return arrayGammaRate;
@@ -30,6 +32,32 @@ const epsilonRate = (sonarInput: sonarInput): epsilonRate => {
     );
 }
 
-const powerConsumption = parseInt(gammaRate(input).join(''), 2) * parseInt(epsilonRate(input).join(''), 2);
+const computedGammaRate = gammaRate(input);
+const computedEpsilonRate = epsilonRate(input);
 
+const powerConsumption = parseInt(computedGammaRate.join(''), 2) *
+                         parseInt(computedEpsilonRate.join(''), 2);
+
+// First part
 console.log(powerConsumption);
+
+
+const oxygenRate = (sonarInput: sonarInput, evaluateFN: Function = gammaRate): oxygenRate => {
+    let oxygenRate = [...sonarInput];
+
+    let i = 0;
+    while (oxygenRate.length > 1) {
+        const tmpRate = evaluateFN(oxygenRate);
+        oxygenRate = oxygenRate.filter(vs => parseInt(vs[i]) === tmpRate[i])
+        i++;
+    }
+
+    return oxygenRate[0].split('').map(x => parseInt(x));
+}
+
+const CO2Rate = (sonarInput: sonarInput): CO2Rate => {
+    return oxygenRate(sonarInput, epsilonRate);
+}
+
+// Second part
+console.log( parseInt(oxygenRate(input).join(''), 2) * parseInt(CO2Rate(input).join(''), 2));
